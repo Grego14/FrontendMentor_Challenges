baa5049724c419818f43cf139f85b0e1e4a3ba9a
Gregorio <gre208981@gmail.com>
2024-04-16 11:25:32 -0400

    move directories

--- .learning_paths/javascript-fundamentals/article-preview-component-master/index.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('share-button')
-const shareLinks = document.getElementById('share-links')
-
-button.addEventListener('click', function(e){
-	this.classList.toggle('active')
-	shareLinks.classList.toggle('active')
-})
