7c0a21017fa5a089184df1fad97d3b0b7e887bb7
Gregorio <gre208981@gmail.com>
2024-05-17 19:25:53 -0400

    refactor code, cool transition with opacity after the sources load, and handle user select and house select options

--- rock-paper-scissors-master/script.js
+++ rock-paper-scissors-master/script.js
@@ -1,27 +1,30 @@
 const d = document
 const w = window
 
-d.addEventListener('DOMContentLoaded', e => {
+d.addEventListener('DOMContentLoaded', async e => {
   const storage = w.localStorage
 
+  const gameElement = d.getElementById('game')
   const template = d.getElementById('game-template').content
   const fragment = d.createDocumentFragment()
-  let score = 12
+  let score = storage.getItem('score') || 12
+
+  const gameImageElement = d.getElementById('game-image')
+  const gameImageAlt = storage.getItem('game-mode') === 'rps'
+    ? 'Rock, paper, scissors'
+    : 'Rock, paper, scissors, lizard, spock'
 
   // the orden of each option is important
   // as we are using nth-child to position them;
   const options = new Map([
-    ['paper', {name: 'paper', image: './assets/images/icon-paper.svg'}],
-    ['scissors', {name: 'scissors', image: './assets/images/icon-scissors.svg'}],
-    ['rock' ,{name: 'rock', image: './assets/images/icon-rock.svg'}],
+    ['paper', {name: 'paper', image: './assets/images/icon-paper.svg', gradient: 'game__option--paper'}],
+    ['scissors', {name: 'scissors', image: './assets/images/icon-scissors.svg', gradient: 'game__option--scissors'}],
+    ['rock' ,{name: 'rock', image: './assets/images/icon-rock.svg', gradient: 'game__option--rock'}],
   ])
 
-  if(!storage.getItem('game-mode')) storage.setItem('game-mode', 'rps') 
-  if(!storage.getItem('score')) storage.setItem('score', score) 
-
-  const backgrounds = {
-    triangle: {main: './assets/images/bg-triangle.svg', rules: './assets/images/image-rules.svg'},
-    pentagon: {main: './assets/images/bg-pentagon.svg', rules: './assets/images/image-rules-bonus.svg'}
+  const images = {
+    triangle: {main: './assets/images/bg-triangle.svg', rules: './assets/images/image-rules.svg', title: './assets/images/logo.svg'},
+    pentagon: {main: './assets/images/bg-pentagon.svg', rules: './assets/images/image-rules-bonus.svg', title: './assets/images/logo-bonus.svg'}
   }
 
   const rules = {
@@ -30,55 +33,104 @@ d.addEventListener('DOMContentLoaded', e => {
     scissors beats lizard, lizard beats paper, paper beats spock, spock beats rock, rock beats scissors`
   }
 
-  const gameBackground = d.getElementById('game-background')
+  if(!storage.getItem('game-mode')) storage.setItem('game-mode', 'rps') 
+  if(!storage.getItem('score')) storage.setItem('score', score) 
+
+  const gameImageUrl = storage.getItem('game-mode') === 'rps'
+    ? images.triangle.title
+    : images.pentagon.title
+
+  gameImageElement.src = await fetch(gameImageUrl).then(res => res.url)
+  gameImageElement.setAttribute('alt', gameImageAlt)
+
+  gameImageElement.addEventListener('load', e => {
+    gameImageElement.classList.add('game__image--loaded')
+  }, {once: true})
+
+  const gameBackgroundElement = d.getElementById('game-background')
   const rulesText = d.getElementById('rules-text')
   const gameContent = d.getElementById('game-content')
 
-  if(storage.getItem('game-mode') === 'rps'){
-    gameBackground.src = backgrounds.triangle.main
-    rulesText.setAttribute('alt', rules.rps)
-    rulesText.setAttribute('src', backgrounds.triangle.rules)
-  }else if(storage.getItem('game-mode') === 'rpsls'){
-    gameBackground.src = backgrounds.pentagon.main
-    rulesText.setAttribute('alt', rules.rpsls)
-    rulesText.setAttribute('src', backgrounds.pentagon.rules)
-    gameContent.classList.add('game__content--rpsls')
+  const backgroundImageUrl = storage.getItem('game-mode') === 'rps'
+  ? images.triangle.main
+  : images.pentagon.main
 
-    options.set('lizard', {name: 'lizard', image: './assets/images/icon-lizard.svg'})
-    options.set('spock', {name: 'spock', image: './assets/images/icon-spock.svg'})
+  gameBackgroundElement.src = await fetch(backgroundImageUrl).then(res => res.url)
+
+  gameBackgroundElement.addEventListener('load', e => {
+    gameBackgroundElement.classList.add('game__background--show')
+  }, {once: true})
+
+  if(storage.getItem('game-mode') === 'rps'){
+
+    rulesText.setAttribute('alt', rules.rps)
+    rulesText.setAttribute('src', images.triangle.rules)
+
+  }else if(storage.getItem('game-mode') === 'rpsls'){
+
+    rulesText.setAttribute('alt', rules.rpsls)
+    rulesText.setAttribute('src', images.pentagon.rules)
+    gameElement.classList.add('game__content--rpsls')
+
+    options.set('lizard', {name: 'lizard', image: './assets/images/icon-lizard.svg', gradient: 'game__option--lizard'})
+    options.set('spock', {name: 'spock', image: './assets/images/icon-spock.svg', gradient: 'game__option--spock'})
   }
 
-  for (const [key, option] of options) {
+  function createOption({name, image, label, gradient, customClass, customAnimation}){
     const clone = template.cloneNode(true)
     const cloneImg = clone.getElementById('image')
-    const cloneName = clone.getElementById('name')
+    const cloneOption = clone.getElementById('name')
 
     cloneImg.removeAttribute('id')
-    cloneName.removeAttribute('id')
+    cloneOption.removeAttribute('id')
+
+    cloneOption.classList.add(gradient)
+
+    if(customClass) cloneOption.classList.add(customClass)
+    if(customAnimation) {
+
+      cloneOption.classList.add(customAnimation)
+
+      cloneOption.addEventListener('animationend', e => {
+        cloneOption.classList.add('game__option--show', 'game__option--start')
+
+        cloneOption.classList.remove(customAnimation)
+      }, {once: true})
+    }
 
     setTimeout(() => {
-      cloneName.classList.add('game__option--show')
+      cloneOption.classList.add('game__option--show', 'game__option--start')
     }, 500);
 
-    cloneName.addEventListener('transitionend', e => {
+    cloneOption.addEventListener('transitionend', e => {
       cloneImg.classList.add('image--show')
     }, {once: true})
 
-    cloneImg.setAttribute('src', option.image)
-    cloneName.setAttribute('aria-label', `Select ${option.name}`)
+    cloneImg.setAttribute('src', image)
+    cloneOption.setAttribute('aria-label', `${label}`)
 
-    fragment.appendChild(clone)
+    return cloneOption
+  }
+
+  for (const [key, option] of options) {
+    const opt = createOption({
+      name: option.name,
+      label: `Select ${option.name}`,
+      image: option.image,
+      gradient: option.gradient
+    })
+
+    fragment.append(opt)
   }
 
   const gameOptions = d.getElementById('game-options')
-
   gameOptions.append(fragment)
 
   const rulesBtn = d.getElementById('game-rules')
   const closeRulesBtn = d.getElementById('close-rules')
-  const rulesOverlay = d.getElementById('game-rules-overlay')
+  const gameRulesOverlay = d.getElementById('game-rules-overlay')
 
-  function handleRulesBtnClick(e){ rulesOverlay.classList.toggle('game__rules--show') }
+  function handleRulesBtnClick(e){ gameRulesOverlay.classList.toggle('game__rules--show') }
 
   function handleOptionsOnClick(clickedOption){
     const domOptions = d.querySelectorAll('.game__option')
@@ -86,25 +138,41 @@ d.addEventListener('DOMContentLoaded', e => {
 
       if(domOpt === clickedOption) continue
 
-      domOpt.classList.remove('game__option--show')
+      domOpt.classList.remove('game__option--show', 'game__option--start')
 
       domOpt.addEventListener('transitionend', e => {
         if(e.propertyName === 'opacity') domOpt.classList.add('game__option--hide')
       }, {once: true})
 
-      gameBackground.classList.add('game__background--hide')
+      gameBackgroundElement.classList.add('game__background--hide')
 
       clickedOption.classList.add('game__option--selected')
-
-      options.set('house', {name: getRandomOption(), image: `./assets/images/icon-${getRandomOption()}.svg`})
-      gameOptions.append(options.get('house'))
     }
+
+    handleHouseOption()
   }
 
-  function handleHouseOption(name, image){
-    const opt = d.createElement('div')
-    opt.classList.add('game__option game__option--house')
+  function handleHouseOption(){
+    const bg = d.createElement('div')
+    bg.classList.add('game__option--house-selecting')
 
+    gameOptions.append(bg)
+
+    const name = getRandomOption()
+
+    const houseOpt = createOption({
+      name: name, 
+      image: `./assets/images/icon-${name}.svg`,
+      label: `The house selects ${name}`,
+      gradient: `game__option--${name}`,
+      customClass: 'game__option--house'
+    })
+
+    houseOpt.addEventListener('transitionend', e =>{
+      gameOptions.removeChild(bg)
+    }, {once: true})
+
+    gameOptions.append(houseOpt)
   }
 
   function getRandomOption(){
@@ -115,16 +183,10 @@ d.addEventListener('DOMContentLoaded', e => {
     return opts[Math.floor(Math.random() * opts.length)]
   }
 
-  function selectOption(e){
-    console.log(e)
-    handleOptionsOnClick(e.target)
-  }
-
-
   d.addEventListener('click', e => {
     if(e.target === rulesBtn) handleRulesBtnClick(e)
     if(e.target === closeRulesBtn) handleRulesBtnClick(e)
 
-    if(e.target.matches('.game__option')) selectOption(e)
+    if(e.target.matches('.game__option')) handleOptionsOnClick(e.target)
   })
 })
