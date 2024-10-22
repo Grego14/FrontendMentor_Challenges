b255f520296ca5518965fde7884f00d22f13979d
Gregorio <gre208981@gmail.com>
2024-10-22 11:34:14 -0400

    update url manually again... change vite config

--- challenges/product-list-with-cart-main/vite.config.js
+++ challenges/product-list-with-cart-main/vite.config.js
@@ -4,15 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   if (command === 'build') {
-    console.log('serving for production...')
     return {
       plugins: [react()],
       build: {
         cssMinify: 'lightningcss',
         minify: true,
-        outDir: 'build',
-        root: '/product-list-with-cart-main/'
-      }
+        outDir: 'build'
+      },
+      base: './'
     }
   }
 
