eb3b36180fe06c39186259b1a26e74a9d04219f9
Gregorio <gre208981@gmail.com>
2024-10-28 14:02:37 -0400

    improve code readability using objects and functions instead of a switch statement

--- challenges/product-list-with-cart-main/src/reducers/productsReducer.js
+++ challenges/product-list-with-cart-main/src/reducers/productsReducer.js
@@ -1,83 +1,84 @@
 export default function productsReducer(state, action) {
-  switch (action.type) {
-    case 'cart': {
-      const map = new Map(state)
-      const id = action.id
-      const element = state.get(id)
-
-      if (!element) return state
-
-      const elementProps = { ...element, cart: !element.cart }
-      // if elementProps.cart is false it means the product is being removed
-      // so count should be reset to 0initial should be false otherwise
-      // the initial animation will be fired once added again.
-      const count = elementProps.cart ? elementProps.count + 1 : 0
-      const outOfStock = count >= elementProps.quantity
-
-      map.set(id, {
-        ...elementProps,
-
-        count: count,
-        // initial should be false otherwise the initial animation will
-        // be fired once added again.
-        initial: false,
-        outOfStock
-      })
-
-      return map
-    }
-
-    case 'increment':
-    case 'decrement': {
-      const id = action.id
-      const element = state.get(id)
-      const map = new Map(state)
-      const userAction = action.type
-
-      if (!element) return state
-
-      let count = element.count
-
-      let outOfStock = count >= element.quantity
-
-      // prevents unnecessary rendering
-      if (outOfStock && userAction !== 'decrement') return state
-
-      userAction === 'increment' ? count++ : count--
-      outOfStock = count >= element.quantity
-
-      // compare to 0 because above we are decrementing no matter what
-      // and if it was 1 now is 0
-      if (userAction === 'decrement' && count === 0) {
-        element.cart = false
-        outOfStock = false
-      }
-
-      const elementProps = { ...element, count, outOfStock }
-
-      map.set(id, elementProps)
-      return map
-    }
-
-    case 'fetch':
-      return new Map(action.products)
-
-    case 'newOrder': {
-      const map = new Map()
-
-      for (const product of [...state.values()]) {
-        map.set(product.id, {
-          ...product,
-          cart: false,
-          count: 0,
-          outOfStock: false
-        })
-      }
-
-      return map
-    }
-
-    default:
-      return state
+  const types = {
+    cart: handleProductInCart,
+    decrement: handleProductQuantity,
+    increment: handleProductQuantity,
+    fetch: handleFetch,
+    newOrder: handleNewOrder
   }
+
+  const id = action.id
+  const element = state.get(id)
+
+  // fetch and newOrder types doesn't have element and id
+  if (!element && action.type !== 'fetch' && action.type !== 'newOrder')
+    return state
+
+  function handleProductInCart() {
+    const map = new Map(state)
+    const elementProps = { ...element, cart: !element.cart }
+
+    // if elementProps.cart is false it means the product
+    // is being removed so count should be reset to 0.
+    const count = elementProps.cart ? elementProps.count + 1 : 0
+    const outOfStock = count >= elementProps.quantity
+
+    map.set(id, {
+      ...elementProps,
+      count: count,
+      // initial should be false otherwise the initial animation will
+      // be fired once added again.
+      initial: false,
+      outOfStock
+    })
+
+    return map
+  }
+
+  function handleProductQuantity() {
+    const map = new Map(state)
+
+    const userAction = action.type
+    let count = element.count
+    let outOfStock = count >= element.quantity
+
+    // prevents unnecessary rendering
+    if (outOfStock && userAction !== 'decrement') return
+
+    userAction === 'increment' ? count++ : count--
+    outOfStock = count >= element.quantity
+
+    // compare to 0 because above we are decrementing no matter what
+    // and if it was 1 now is 0
+    if (userAction === 'decrement' && count === 0) {
+      element.cart = false
+      outOfStock = false
+    }
+
+    const elementProps = { ...element, count, outOfStock }
+
+    map.set(id, elementProps)
+    return map
+  }
+
+  function handleFetch() {
+    return new Map(action.products)
+  }
+
+  function handleNewOrder() {
+    const map = new Map()
+
+    for (const product of [...state.values()]) {
+      map.set(product.id, {
+        ...product,
+        cart: false,
+        count: 0,
+        outOfStock: false
+      })
+    }
+
+    return map
+  }
+
+  return types[action.type]() || state
 }
