02bd66816b58d2a4ae1d5230ee8daa29ec7c0944
Gregorio <gre208981@gmail.com>
2024-10-28 17:54:22 -0400

    add build files

---
+++ challenges/product-list-with-cart-main/build/assets/TotalPrice-Df7gg3QJ.js
@@ -0,0 +1,1 @@
+import{j as a,t as r}from"./index-BfsSDj0Q.js";function c({price:s,discount:e,amount:n}){return a.jsxs("div",{className:"total-price-container",children:[a.jsx("span",{className:"price",children:r(s)}),e&&a.jsx("span",{className:"discount-price",children:r(s-s/100*(n||10))})]})}export{c as T};
