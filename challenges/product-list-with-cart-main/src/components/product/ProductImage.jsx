01f830b01eddff851e946e5dcb3f680db4c9d688
Gregorio <gre208981@gmail.com>
2024-10-29 20:54:49 -0400

    update transition values

--- challenges/product-list-with-cart-main/src/components/product/ProductImage.jsx
+++ challenges/product-list-with-cart-main/src/components/product/ProductImage.jsx
@@ -6,13 +6,12 @@ export default function ProductImage({ images, onCart, setImageLoaded, show }) {
   }
 
   const imageVariants = {
-    hidden: { opacity: 0, scale: 0 },
+    hidden: { opacity: 0, scale: 0.5 },
     show: {
       opacity: 1,
       scale: 1,
       transition: {
-        delay: 0.2,
-        duration: 0.3
+        duration: 0.2
       }
     }
   }
