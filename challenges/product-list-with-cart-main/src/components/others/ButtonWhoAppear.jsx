73774ab7f8e8bd5240fbf8a370996b0b5965744b
Gregorio <gre208981@gmail.com>
2024-11-09 18:38:33 -0400

    Update ButtonWhoAppear component variants

--- challenges/product-list-with-cart-main/src/components/others/ButtonWhoAppear.jsx
+++ challenges/product-list-with-cart-main/src/components/others/ButtonWhoAppear.jsx
@@ -8,17 +8,18 @@ export default memo(function ButtonWhoAppear({
   children,
   text
 }) {
-  const buttonVariants = {
+  const variants = {
     hidden: {
       opacity: 0,
       scale: 0.5
     },
+
     show: {
       opacity: 1,
-      scale: 1,
-
+      scale: [1, 1.15, 1],
       transition: {
-        duration: 0.4,
+        duration: 0.3,
+        delay: 0.2,
         ease: 'easeInOut'
       }
     }
@@ -30,7 +31,7 @@ export default memo(function ButtonWhoAppear({
       initial='hidden'
       whileInView={isVisible ? 'show' : 'hidden'}
       viewport={{ once: true }}
-      variants={props.variants || buttonVariants}
+      variants={props.variants || variants}
       type='button'
       disabled={props?.disabled ? props?.disabled : !isVisible}
       onContextMenu={preventContextMenu}>
