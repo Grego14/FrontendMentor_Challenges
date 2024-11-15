24ae28d6ce6ef4aaa957db6e7c63b89022c23f22
Gregorio <gre208981@gmail.com>
2024-11-14 23:26:00 -0400

    Refactor the code; Use the prop outOfStock to make the increment button non-focusable when it is true; Update some html classes

--- challenges/product-list-with-cart-main/src/components/product/ProductButtons.jsx
+++ challenges/product-list-with-cart-main/src/components/product/ProductButtons.jsx
@@ -3,12 +3,14 @@ import { useId } from 'react'
 import { m } from 'framer-motion'
 
 export function QuantityButtons(props) {
-  const { imageLoaded, onCart, count } = props
+  const { imageLoaded, onCart, count, outOfStock } = props
+  const visible = imageLoaded && onCart
 
   const quantityBtnProps = {
     className: 'product__button product__button__quantity',
-    tabIndex: onCart ? '0' : '-1',
-    visible: imageLoaded && onCart
+    tabIndex: visible ? '0' : '-1',
+    visible,
+    outOfStock
   }
 
   const variants = {
@@ -30,11 +32,11 @@ export function QuantityButtons(props) {
   return (
     <m.div
       initial='hidden'
-      whileInView={imageLoaded && onCart ? 'show' : ''}
+      whileInView={visible ? 'show' : ''}
       viewport={{ once: true }}
       variants={variants}
-      className='product__buttons__quantity pos-absolute'
-      aria-hidden={onCart ? 'false' : 'true'}>
+      aria-hidden={visible}
+      className='product__buttons__quantity pos-absolute'>
       <QuantityButton props={quantityBtnProps} type='decrement' />
 
       <div
@@ -49,12 +51,15 @@ export function QuantityButtons(props) {
 }
 
 function QuantityButton({ props, type }) {
-  const { visible, ...buttonProps } = props
+  const { visible, tabIndex, outOfStock, ...buttonProps } = props
 
   const _props = {
     ...buttonProps,
     'aria-label': `${type} product count`,
-    'data-action': type
+    'data-action': type,
+
+    // prevent focusing on the increment button when the product is out of stock
+    tabIndex: type !== 'increment' || !outOfStock ? tabIndex : '-1'
   }
 
   const decrementIcon = (
@@ -66,7 +71,7 @@ function QuantityButton({ props, type }) {
       fill='none'
       viewBox='0 0 10 2'>
       <title>Decrement product count</title>
-      <path fill='#fff' d='M0 .375h10v1.25H0V.375Z' />
+      <path className='quantity-path' fill='#fff' d='M0 .375h10v1.25H0V.375Z' />
     </svg>
   )
 
@@ -80,6 +85,7 @@ function QuantityButton({ props, type }) {
       viewBox='0 0 10 10'>
       <title>Increment product count</title>
       <path
+        className='quantity-path'
         fill='#fff'
         d='M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z'
       />
@@ -87,50 +93,59 @@ function QuantityButton({ props, type }) {
   )
 
   return (
-    <ButtonWhoAppear props={_props} isVisible={visible}>
-      {type === 'increment' ? incrementIcon : decrementIcon}
-    </ButtonWhoAppear>
+    <ButtonWhoAppear
+      props={_props}
+      isVisible={visible}
+      render={type === 'increment' ? incrementIcon : decrementIcon}
+    />
   )
 }
 
 export function AddToCartButton(props) {
-  const onCart = props.onCart
+  const visible = props.imageLoaded && !props.onCart
   const id = useId()
 
   const addToCartProps = {
-    className: 'product__button product__button--add pos-absolute',
-    tabIndex: onCart ? '-1' : '0',
-    'aria-hidden': onCart ? 'true' : 'false',
-    'data-action': 'cart'
+    className: 'product__button product__button__add pos-absolute',
+    tabIndex: visible ? '0' : '-1',
+    'data-action': 'cart',
+    'data-no-offset': '',
+    'aria-hidden': !visible
   }
-
-  const addToCartIcon = (
-    <svg
-      aria-hidden='true'
-      xmlns='http://www.w3.org/2000/svg'
-      width='21'
-      height='20'
-      fill='none'
-      viewBox='0 0 21 20'>
-      <title>add to cart</title>
-      <g fill='#C73B0F' clipPath={`url(#${id})`}>
-        <path d='M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z' />
-        <path d='M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z' />
-      </g>
-      <defs>
-        <clipPath id={id}>
-          <path fill='#fff' d='M.333 0h20v20h-20z' />
-        </clipPath>
-      </defs>
-    </svg>
-  )
 
   return (
     <ButtonWhoAppear
       props={addToCartProps}
-      isVisible={props.imageLoaded && !onCart}>
-      {addToCartIcon}
-      <span className='button--add__text'>Add to Cart</span>
-    </ButtonWhoAppear>
+      isVisible={visible}
+      render={() => {
+        return (
+          <>
+            <svg
+              className='button__add__svg'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              width='21'
+              height='20'
+              fill='none'
+              viewBox='0 0 21 20'>
+              <title>add to cart</title>
+              <g
+                className='button__add__fill'
+                fill='#C73B0F'
+                clipPath={`url(#${id})`}>
+                <path d='M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z' />
+                <path d='M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z' />
+              </g>
+              <defs>
+                <clipPath id={id}>
+                  <path fill='#fff' d='M.333 0h20v20h-20z' />
+                </clipPath>
+              </defs>
+            </svg>
+            <span className='button--add__text'>Add to Cart</span>
+          </>
+        )
+      }}
+    />
   )
 }
