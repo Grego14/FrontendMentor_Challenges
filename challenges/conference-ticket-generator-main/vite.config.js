1e6454eecb2c87dadd1c4fe25e67b3ef9f160da5
Gregorio <gre208981@gmail.com>
2025-02-09 13:53:49 -0400

    Update viteconfig

--- challenges/conference-ticket-generator-main/vite.config.js
+++ challenges/conference-ticket-generator-main/vite.config.js
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig((command) => {
+  if (command === 'build') {
+    return {
+      plugins: [react()],
+      build: {
+        cssMinify: 'lightningcss',
+        minify: true,
+        outDir: 'build'
+      },
+      base: './'
+    }
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true
+    },
+    base: './'
+  }
 })
