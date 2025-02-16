73fc98e9fd1c3252f81a3d725737f412bcaf01d3
Gregorio <gre208981@gmail.com>
2025-02-15 20:05:37 -0400

    Format Ticket component

--- challenges/conference-ticket-generator-main/src/components/ticket/Ticket.jsx
+++ challenges/conference-ticket-generator-main/src/components/ticket/Ticket.jsx
@@ -3,18 +3,24 @@ import { substring, BASE_URL } from '../../utils/utils'
 import './Ticket.css'
 
 const Ticket = forwardRef((props, ref) => {
-  const { ticketVisible, fullName = '', githubUser = '', userAvatar,
-    setTicketID, ticketID } = props
+  const {
+    ticketVisible,
+    fullName,
+    githubUser,
+    userAvatar,
+    setTicketID,
+    ticketID
+  } = props
 
   useEffect(() => {
     setTicketID(Math.floor(Math.random() * 10000) + 1)
   }, [setTicketID])
 
   return (
-    <div ref={ref}
+    <div
+      ref={ref}
       className={`ticket pos-absolute${ticketVisible ? ' ticket--show' : ''}`}
-      aria-hidden={!ticketVisible}
-    >
+      aria-hidden={!ticketVisible}>
       <div className='ticket__content-left'>
         <div>
           <img
@@ -23,7 +29,8 @@ const Ticket = forwardRef((props, ref) => {
             alt=''
             aria-hidden='true'
             width='40'
-            height='40' />
+            height='40'
+          />
           <div className='data__top__footer'>
             <h2 className='ticket__title'>Coding Conf</h2>
             <span className='ticket__date'>
@@ -39,7 +46,8 @@ const Ticket = forwardRef((props, ref) => {
             alt={`${fullName} avatar picture`}
             aria-hidden='true'
             width='50'
-            height='50' />
+            height='50'
+          />
 
           <div className='data__bottom__footer'>
             <h3 className='ticket__full-name'>{substring(fullName, 10)}</h3>
@@ -48,21 +56,26 @@ const Ticket = forwardRef((props, ref) => {
               href={`https://github.com/${githubUser.split('@')[1]}`}
               target='_blank'
               rel='noreferrer'
-              aria-label={`${fullName} github profile`} >
+              aria-label={`${fullName} github profile`}>
               <img
                 className='github-link__icon'
                 src={`${BASE_URL}assets/images/icon-github.svg`}
                 alt=''
                 width='20'
-                height='20' />
-              <span className='github-link__name'>{substring(githubUser, 12)}</span>
+                height='20'
+              />
+              <span className='github-link__name'>
+                {substring(githubUser, 12)}
+              </span>
             </a>
           </div>
         </div>
       </div>
 
       <div className='ticket__content-right'>
-        <div className='ticket__id'>#{ticketID?.toString().padStart(5, '0')}</div>
+        <div className='ticket__id'>
+          #{ticketID?.toString().padStart(5, '0')}
+        </div>
       </div>
     </div>
   )
